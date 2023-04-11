d9959657228854a3049294dd5292893fd6e4e3ac
Darkland <darlan.1100@gmail.com>
2023-04-11 17:58:24 -0600

    Landing page animation fix

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.css']
+  styleUrls: ['./main-menu.component.css'],
 })
 export class MainMenuComponent implements OnInit {
+  constructor(private elRef: ElementRef) {}
 
-  constructor() { }
+  ngAfterViewInit() {
+    const leftDiv = this.elRef.nativeElement.querySelector('.left');
+    const rightDiv = this.elRef.nativeElement.querySelector('.right');
+    const middleDiv = this.elRef.nativeElement.querySelector('.middle');
 
-  ngOnInit(): void {
+    rightDiv.addEventListener('mouseover', () => {
+      leftDiv.style.width = '30vw';
+      rightDiv.style.width = '70vw';
+      middleDiv.style.top = '-25%';
+    });
+
+    leftDiv.addEventListener('mouseover', () => {
+      leftDiv.style.width = '70vw';
+      rightDiv.style.width = '30vw';
+      middleDiv.style.top = '-25%';
+    });
+
+    rightDiv.addEventListener('mouseleave', () => {
+      leftDiv.style.width = '50vw';
+      rightDiv.style.width = '50vw';
+      middleDiv.style.top = '5%';
+    });
+
+    leftDiv.addEventListener('mouseleave', () => {
+      leftDiv.style.width = '50vw';
+      rightDiv.style.width = '50vw';
+      middleDiv.style.top = '5%';
+    });
   }
 
+  ngOnInit(): void {}
 }
