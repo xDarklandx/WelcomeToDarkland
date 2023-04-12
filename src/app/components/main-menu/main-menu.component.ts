4f8c81ae3603e1fcacde9449b23904b0c93878ec
Darkland <darlan.1100@gmail.com>
2023-04-12 17:58:56 -0600

    more animations button change pending

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -6,36 +6,56 @@ import { Component, OnInit, ElementRef } from '@angular/core';
   styleUrls: ['./main-menu.component.css'],
 })
 export class MainMenuComponent implements OnInit {
+  buttonVisible: boolean = false;
+  buttonVisibleInterval: any;
+
   constructor(private elRef: ElementRef) {}
 
   ngAfterViewInit() {
+    const container = this.elRef.nativeElement.querySelector('.container');
     const leftDiv = this.elRef.nativeElement.querySelector('.left');
     const rightDiv = this.elRef.nativeElement.querySelector('.right');
     const middleDiv = this.elRef.nativeElement.querySelector('.middle');
+    // const button = this.elRef.nativeElement.querySelector('.button');
+    // const buttons = this.elRef.nativeElement.querySelectorAll('.button');
 
-    rightDiv.addEventListener('mouseover', () => {
-      leftDiv.style.width = '30vw';
-      rightDiv.style.width = '70vw';
-      middleDiv.style.top = '-25%';
+    // button.style.display = 'none';
+
+    container.addEventListener('mouseover', (event: MouseEvent) => {
+      if (event.target === leftDiv) {
+        leftDiv.style.width = '70vw';
+        rightDiv.style.width = '30vw';
+        middleDiv.style.top = '-25%';
+        this.buttonVisible = true;
+      } else if (event.target === rightDiv) {
+        leftDiv.style.width = '30vw';
+        rightDiv.style.width = '70vw';
+        middleDiv.style.top = '-25%';
+        this.buttonVisible = true;
+      }
     });
 
-    leftDiv.addEventListener('mouseover', () => {
-      leftDiv.style.width = '70vw';
-      rightDiv.style.width = '30vw';
-      middleDiv.style.top = '-25%';
-    });
-
-    rightDiv.addEventListener('mouseleave', () => {
+    container.addEventListener('mouseleave', () => {
       leftDiv.style.width = '50vw';
       rightDiv.style.width = '50vw';
       middleDiv.style.top = '5%';
+      this.buttonVisible = false;
     });
 
-    leftDiv.addEventListener('mouseleave', () => {
-      leftDiv.style.width = '50vw';
-      rightDiv.style.width = '50vw';
-      middleDiv.style.top = '5%';
-    });
+    // toggle button visibility
+    // this.buttonVisibleInterval = setInterval(() => {
+    //   for (let i = 0; i < buttons.length; i++) {
+    //     if (this.buttonVisible) {
+    //       buttons[i].classList.remove('hidden');
+    //     } else {
+    //       buttons[i].classList.add('hidden');
+    //     }
+    //   }
+    // }, 100);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.buttonVisibleInterval);
   }
 
   ngOnInit(): void {}
